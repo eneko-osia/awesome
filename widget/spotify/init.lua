f31da65cb22cf9a100d81e1d99d50c14bc59aad9
eneko-osia <eneko.osia@gmail.com>
2024-09-04 21:35:49 +0200

    spotify widget updated

--- widget/spotify/init.lua
+++ widget/spotify/init.lua
@@ -11,110 +11,108 @@ local wibox         = require("wibox")
 local function factory(args)
     args = args or {}
 
-    local icons = args.icons or {
-        logo = nil,
-        next = nil,
-        pause = nil,
-        play = nil,
-        prev = nil
-    }
+    local icons = args.icons or
+        {
+            logo = nil,
+            next = nil,
+            pause = nil,
+            play = nil,
+            prev = nil
+        }
     local song_index = 0
     local song_state = 0
     local song_text = "Spotify"
 
-    local spotify =
+    local spotify = wibox.widget(
         {
-            widget = wibox.widget(
+            {
                 {
                     {
                         {
-                            {
-                                {
-                                    widget = wibox.widget.imagebox(icons.logo)
-                                },
-                                layout = wibox.container.margin(_, 4, 4, 3, 3)
-                            },
-                            {
-                                {
-                                    {
-                                        align  = "center",
-                                        forced_width = beautiful_dpi(256),
-                                        text = song_text,
-                                        valign = "center",
-                                        widget = wibox.widget.textbox
-                                    },
-                                    bg = beautiful.bg_focus,
-                                    shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 2) end,
-                                    widget = wibox.container.background
-                                },
-                                layout = wibox.container.margin(_, _, 4, 3, 3)
-                            },
-                            layout = wibox.layout.fixed.horizontal
+                            widget = wibox.widget.imagebox(icons.logo)
                         },
-                        bg = beautiful.bg_reset,
-                        shape = gears.shape.rectangle,
-                        widget = wibox.container.background
-                    },
-                    {
-                        {
-                            widget = wibox.widget.imagebox(beautiful.spr)
-                        },
-                        layout = wibox.container.margin(_, _, _, _, _)
+                        layout = wibox.container.margin(_, 4, 4, 3, 3)
                     },
                     {
                         {
                             {
-                                widget = wibox.widget.imagebox(icons.prev)
+                                align  = "center",
+                                forced_width = beautiful_dpi(256),
+                                text = song_text,
+                                valign = "center",
+                                widget = wibox.widget.textbox
                             },
-                            layout = wibox.container.margin(_, 4, 4, 3, 3)
+                            bg = beautiful.bg_focus,
+                            shape = function(cr, width, height) gears.shape.rounded_rect(cr, width, height, 2) end,
+                            widget = wibox.container.background
                         },
-                        bg = beautiful.bg_reset,
-                        shape = gears.shape.rectangle,
-                        widget = wibox.container.background
-                    },
-                    {
-                        {
-                            widget = wibox.widget.imagebox(beautiful.spr)
-                        },
-                        layout = wibox.container.margin(_, _, _, _, _)
-                    },
-                    {
-                        {
-                            {
-                                widget = wibox.widget.imagebox(icons.play)
-                            },
-                            layout = wibox.container.margin(_, 4, 4, 3, 3)
-                        },
-                        bg = beautiful.bg_reset,
-                        shape = gears.shape.rectangle,
-                        widget = wibox.container.background
-                    },
-                    {
-                        {
-                            widget = wibox.widget.imagebox(beautiful.spr)
-                        },
-                        layout = wibox.container.margin(_, _, _, _, _)
-                    },
-                    {
-                        {
-                            {
-                                widget = wibox.widget.imagebox(icons.next)
-                            },
-                            layout = wibox.container.margin(_, 4, 4, 3, 3)
-                        },
-                        bg = beautiful.bg_reset,
-                        shape = gears.shape.rectangle,
-                        widget = wibox.container.background
+                        layout = wibox.container.margin(_, _, 4, 3, 3)
                     },
                     layout = wibox.layout.fixed.horizontal
-                }
-            )
+                },
+                bg = beautiful.bg_reset,
+                shape = gears.shape.rectangle,
+                widget = wibox.container.background
+            },
+            {
+                {
+                    widget = wibox.widget.imagebox(beautiful.spr)
+                },
+                layout = wibox.container.margin(_, _, _, _, _)
+            },
+            {
+                {
+                    {
+                        widget = wibox.widget.imagebox(icons.prev)
+                    },
+                    layout = wibox.container.margin(_, 4, 4, 3, 3)
+                },
+                bg = beautiful.bg_reset,
+                shape = gears.shape.rectangle,
+                widget = wibox.container.background
+            },
+            {
+                {
+                    widget = wibox.widget.imagebox(beautiful.spr)
+                },
+                layout = wibox.container.margin(_, _, _, _, _)
+            },
+            {
+                {
+                    {
+                        widget = wibox.widget.imagebox(icons.play)
+                    },
+                    layout = wibox.container.margin(_, 4, 4, 3, 3)
+                },
+                bg = beautiful.bg_reset,
+                shape = gears.shape.rectangle,
+                widget = wibox.container.background
+            },
+            {
+                {
+                    widget = wibox.widget.imagebox(beautiful.spr)
+                },
+                layout = wibox.container.margin(_, _, _, _, _)
+            },
+            {
+                {
+                    {
+                        widget = wibox.widget.imagebox(icons.next)
+                    },
+                    layout = wibox.container.margin(_, 4, 4, 3, 3)
+                },
+                bg = beautiful.bg_reset,
+                shape = gears.shape.rectangle,
+                widget = wibox.container.background
+            },
+            layout = wibox.layout.fixed.horizontal
         }
+    )
 
     -- methods
     local function set_song_text(state, text, index)
         local function _set(color, text)
-            local song_widget_container = spotify.widget:get_children()[1]
+            local song_widget_container = spotify:get_children()[1]
             local song_text_widget_container = song_widget_container:get_children()[1]:get_children()[2]:get_children()[1]
             local song_text_widget = song_text_widget_container:get_children()[1]
             song_text_widget:set_markup("<span foreground='" .. color .. "'>" .. text .. "</span>")
@@ -135,7 +133,7 @@ local function factory(args)
             "sp current-oneline",
             function(stdout, _, _, _)
                 local text = "   " .. stdout:sub(1, stdout:len() - 1) .. "   "
-                if (song_text ~= text) then
+                if song_text ~= text then
                     song_index = 1
                     song_text = text
                 end
@@ -147,12 +145,12 @@ local function factory(args)
             "sp status",
             function(stdout, _, _, _)
                 local function _set_play_pause_icon(icon)
-                    local play_pause_icon_widget_container = spotify.widget:get_children()[5]
+                    local play_pause_icon_widget_container = spotify:get_children()[5]
                     local play_pause_icon_widget = play_pause_icon_widget_container:get_children()[1]:get_children()[1]
                     play_pause_icon_widget.image = icon
                 end
 
-                if (stdout:gsub("\n", "") == "Playing") then
+                if stdout:gsub("\n", "") == "Playing" then
                     if song_state ~= 1 then
                         song_state = 1
                         _set_play_pause_icon(icons.pause)
@@ -208,7 +206,7 @@ local function factory(args)
     end
 
     -- bindings
-    local next_icon_widget_container = spotify.widget:get_children()[7]
+    local next_icon_widget_container = spotify:get_children()[7]
     next_icon_widget_container:buttons(
         gears.table.join(
             awful.button(
@@ -222,7 +220,7 @@ local function factory(args)
         )
     )
 
-    local play_pause_icon_widget_container = spotify.widget:get_children()[5]
+    local play_pause_icon_widget_container = spotify:get_children()[5]
     play_pause_icon_widget_container:buttons(
             gears.table.join(
             awful.button(
@@ -236,7 +234,7 @@ local function factory(args)
         )
     )
 
-    local prev_icon_widget_container = spotify.widget:get_children()[3]
+    local prev_icon_widget_container = spotify:get_children()[3]
     prev_icon_widget_container:buttons(
         gears.table.join(
             awful.button(
@@ -250,7 +248,7 @@ local function factory(args)
         )
     )
 
-    local song_widget_container = spotify.widget:get_children()[1]
+    local song_widget_container = spotify:get_children()[1]
     local song_text_widget_container = song_widget_container:get_children()[1]:get_children()[2]:get_children()[1]
     local song_text_widget = song_text_widget_container:get_children()[1]
     song_text_widget:buttons(
@@ -258,10 +256,11 @@ local function factory(args)
             awful.button(
                 {},
                 4,
+                _,
                 function()
-                    if (song_state == 1) then
+                    if song_state == 1 then
                         song_index = song_index + 1
-                        if (song_index > song_text:len()) then
+                        if song_index > song_text:len() then
                             song_index = 1
                         end
                         set_song_text(song_state, song_text, song_index)
@@ -272,10 +271,11 @@ local function factory(args)
             awful.button(
                 {},
                 5,
+                _,
                 function()
-                    if (song_state == 1) then
+                    if song_state == 1 then
                         song_index = song_index - 1
-                        if (song_index <= 0) then
+                        if song_index <= 0 then
                             song_index = song_text:len()
                         end
                         set_song_text(song_state, song_text, song_index)
@@ -290,6 +290,7 @@ local function factory(args)
             awful.button(
                 {},
                 1,
+                _,
                 function()
                     if string.find(song_text, "Spotify is not running") ~= nil then
                         awful.spawn.easy_async(
@@ -365,9 +366,9 @@ local function factory(args)
             callback =
                 function()
                     -- update song index if playing
-                    if (song_state == 1) then
+                    if song_state == 1 then
                         song_index = song_index + 1
-                        if (song_index > song_text:len()) then
+                        if song_index > song_text:len() then
                             song_index = 1
                         end
                     else
