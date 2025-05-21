1850d35cba6d33246e868b04a962bce66bc71e21
eneko-osia <eneko@tuta.com>
2025-05-21 23:09:39 +0200

    battery widget updated

--- widget/battery/init.lua
+++ widget/battery/init.lua
@@ -20,7 +20,12 @@ local function factory(args)
             logo = nil,
             low = nil
         }
-    local info = {}
+    local info = 
+        {
+            batteries = {},
+            percentage = 0
+        }
+    local timeout = args.timeout or 2
     local widget = wibox.widget(
         {
             {
@@ -53,117 +58,157 @@ local function factory(args)
         }
     )
 
-    -- lain widget creation
-    local lain_battery = lain.widget.bat({
-        notify = "on",
-        full_notify = "on",
-        settings = function()
-            local icon_widget_container = widget:get_children()[1]:get_children()[1]
-            local icon_widget = icon_widget_container:get_children()[1]
-            local text_widget_container = widget:get_children()[1]:get_children()[2]:get_children()[1]
-            local text_widget = text_widget_container:get_children()[1]
-            if (icon_widget ~= nil and text_widget ~= nil) then
-                if bat_now.ac_status == "N/A" or bat_now.ac_status == 1 then
+    -- methods
+    local function update_battery_widget()
+        -- local netdl_text_widget = widget:get_children_by_id("text_netdl")[1]
+        -- netdl_text_widget:set_markup(convert_bytes_to_bits_per_second(info.received))
+
+        -- local netup_text_widget = widget:get_children_by_id("text_netup")[1]
+        -- netup_text_widget:set_markup(convert_bytes_to_bits_per_second(info.sent))
+
+        local icon_widget_container = widget:get_children()[1]:get_children()[1]
+        local icon_widget = icon_widget_container:get_children()[1]
+        local text_widget_container = widget:get_children()[1]:get_children()[2]:get_children()[1]
+        local text_widget = text_widget_container:get_children()[1]
+        if (icon_widget ~= nil and text_widget ~= nil) then
+            if info.ac.status == true then
+                icon_widget:set_image(icons.ac)
+            --     if bat_now.perc ~= "N/A" then
+            --         notification_text = " " .. bat_now.time .. " "
+            --         text_widget:set_markup(" " .. bat_now.perc .. "%" .. " ")
+            --     else
+            --         notification_text = " " .. bat_now.time .. " "
+            --         text_widget:set_markup(" 100% ")
+            --     end
+            else
+                if info.percentage == 100 then
                     icon_widget:set_image(icons.ac)
-                    if bat_now.perc ~= "N/A" then
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(" " .. bat_now.perc .. "%" .. " ")
-                    else
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(" 100% ")
-                    end
-                elseif bat_now.perc ~= "N/A" then
-                    local bat_perc_number = tonumber(bat_now.perc)
-                    if bat_now.ac_status == 1 or bat_perc_number == 100 then
-                        icon_widget:set_image(icons.ac)
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(" " .. bat_now.perc .. "%" .. " ")
-                    elseif bat_perc_number > 50 then
-                        icon_widget:set_image(icons.full)
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(" " .. bat_now.perc .. "%" .. " ")
-                    elseif bat_perc_number > 15 then
-                        icon_widget:set_image(icons.low)
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(lain.util.markup(beautiful.fg_focus, " " .. bat_now.perc .. "%" .. " "))
-                    else
-                        icon_widget:set_image(icons.empty)
-                        notification_text = " " .. bat_now.time .. " "
-                        text_widget:set_markup(lain.util.markup(beautiful.fg_urgent, " " .. bat_now.perc .. "%" .. " "))
-                    end
+                    -- notification_text = " " .. bat_now.time .. " "
+                    text_widget:set_markup(" " .. info.percentage .. "%" .. " ")
+                elseif info.percentage > 50 then
+                    icon_widget:set_image(icons.full)
+                    -- notification_text = " " .. bat_now.time .. " "
+                    text_widget:set_markup(" " .. info.percentage .. "%" .. " ")
+                elseif info.percentage > 15 then
+                    icon_widget:set_image(icons.low)
+                    -- notification_text = " " .. bat_now.time .. " "
+                    text_widget:set_markup(lain.util.markup(beautiful.fg_focus, " " .. info.percentage .. "%" .. " "))
                 else
                     icon_widget:set_image(icons.empty)
-                    notification_text = " " .. bat_now.time .. " "
-                    text_widget:set_markup(lain.util.markup(beautiful.fg_urgent, " " .. bat_now.perc .. "%" .. " "))
+                    -- notification_text = " " .. bat_now.time .. " "
+                    text_widget:set_markup(lain.util.markup(beautiful.fg_urgent, " " .. info.percentage .. "%" .. " "))
                 end
+            -- else
+            --     icon_widget:set_image(icons.empty)
+            --     notification_text = " " .. bat_now.time .. " "
+            --     text_widget:set_markup(lain.util.markup(beautiful.fg_urgent, " " .. info.percentage .. "%" .. " "))
             end
-        end,
-        widget = widget
-    })
+        end
+    end
+
+    local function update_ac_state()
+        awful.spawn.easy_async(
+            string.format("cat /sys/class/power_supply/%s/online", info.ac.name),
+            function(stdout, _, _, _)
+                -- read data from command results
+                info.ac.status = (tonumber(stdout) == 1)
+            end
+        )
+    end
 
     local function update_battery_state(bat_info)
         awful.spawn.easy_async(
-            string.format("cat /sys/class/power_supply/%s/present", bat_info.name),
+            string.format("cat \
+                /sys/class/power_supply/%s/status \
+                /sys/class/power_supply/%s/energy_now \
+                /sys/class/power_supply/%s/energy_full",
+                bat_info.name,
+                bat_info.name,
+                bat_info.name),
             function(stdout, _, _, _)
+                -- read data from command results
+                local i = 0
+                for v in stdout:gmatch("[^\r\n]+") do
+                    if i == 0 then
+                        bat_info.status = v
+                    elseif i == 1 then
+                        bat_info.energy_now = tonumber(v)
+                    elseif i == 2 then
+                        bat_info.energy_full = tonumber(v)
+                    end
+                    i = i + 1
+                end
+
+                -- calculate percentage
+                bat_info.percentage = math.floor((bat_info.energy_now / bat_info.energy_full) * 100)
+
+                -- update popup
+                -- if net_info.popup.speed_row.visible then
+                --     update_speed_popup(net_info)
+                -- end
+
+                -- calculate total percentage
+                local total_energy_full = 0
+                local total_energy_now = 0
+                for _, v in pairs(info.batteries) do
+                    total_energy_full = total_energy_full + v.energy_full
+                    total_energy_now = total_energy_now + v.energy_now
+                end
+                info.percentage = math.floor((total_energy_now / total_energy_full) * 100)
+
+                -- update widget
+                update_battery_widget()
             end
         )
     end
 
     local function update()
         -- check batteries in the system
-        -- awful.spawn.easy_async(
-        --     "ls /sys/class/power_supply",
-        --     function(stdout, _, _, _)
-        --         -- iterate all batteries
-        --         local i = 1
-        --         for name in stdout:gmatch("[^\r\n]+") do
-        --             if string.match(name, "BAT%w+") then
-        --                 if info.batteries[i] then
-        --                     -- remove battery since it is not the same
-        --                     if name ~= info.batteries[i].name then
-        --                         -- remove_connection_info(i)
-        --                     end
-        --                 end
+        awful.spawn.easy_async(
+            "ls /sys/class/power_supply",
+            function(stdout, _, _, _)
+                -- iterate all batteries
+                local i = 1
+                for name in stdout:gmatch("[^\r\n]+") do
+                    if string.match(name, "BAT%w+") then
+                        if not info.batteries[i] then
+                            -- add new battery info since there isn't any
+                            info.batteries[i] =
+                                {
+                                    name = name,
+                                    energy_full = 0,
+                                    energy_now = 0,
+                                    percentage = 0,
+                                    status = "N/A"
+                                }
+                            -- local bat_info = info.batteries[i]
 
-        --                 if not info.batteries[i] then
-        --                     -- add new battery info since there isn't any
-        --                     info.batteries[i] =
-        --                         {
-        --                             name = name
-        --                         }
-        --                     -- local bat_info = info.batteries[#info.batteries]
+                            -- add popup row to layout
+                            -- popup_speed:get_widget():get_children_by_id("row_container")[1]:add(bat_info.popup.speed_row)
+                        end
 
-        --                     -- add popup row to layout
-        --                     -- popup_speed:get_widget():get_children_by_id("row_container")[1]:add(bat_info.popup.speed_row)
-        --                 end
+                        local bat_info = info.batteries[i]
 
-        --                 local bat_info = info.batteries[i]
+                        -- check battery state
+                        update_battery_state(bat_info)
 
-        --                 -- check battery state
-        --                 update_battery_state(bat_info)
+                        -- continue
+                        i = i + 1
+                    elseif string.match(name, "A%w+") then
+                        if not info.ac then
+                            -- add ac info since there isn't any
+                            info.ac =
+                                {
+                                    name = name
+                                }
+                        end
 
-        --                 -- check interface connection info
-        --                 -- update_connection_info(bat_info)
-
-        --                 -- continue
-        --                 i = i + 1
-        --             elseif string.match(power_supply, "A%w+") then
-        --                 if not info.ac then
-        --                     -- add ac info since there isn't any
-        --                     info.ac =
-        --                         {
-        --                             name = name
-        --                         }
-        --                 end
-        --             end
-        --         end
-
-        --         -- for j = #info.batteries, i, -1 do
-        --         --     -- remove batteries that are not in the system anymore
-        --         --     remove_connection_info(j)
-        --         -- end
-        --     end
-        -- )
+                        -- check battery state
+                        update_ac_state()
+                    end
+                end
+            end
+        )
     end
 
     -- signals
@@ -188,14 +233,14 @@ local function factory(args)
     )
 
     -- timers
-    -- gears.timer(
-    --     {
-    --         autostart = true,
-    --         call_now = true,
-    --         callback = update,
-    --         timeout = timeout
-    --     }
-    -- )
+    gears.timer(
+        {
+            autostart = true,
+            call_now = true,
+            callback = update,
+            timeout = timeout
+        }
+    )
 
     return widget
 end
