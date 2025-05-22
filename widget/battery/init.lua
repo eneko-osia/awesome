bd06f82db4835c84790d9af3641a028d23c9af49
eneko-osia <eneko@tuta.com>
2025-05-22 15:40:40 +0200

    battery widget updated

--- widget/battery/init.lua
+++ widget/battery/init.lua
@@ -6,13 +6,6 @@ local gears         = require("gears")
 local wibox         = require("wibox")
 -- }}}
 
--- {{{ Variable definitions
-local UNKNOWN_TYPE  = 0
-local AC_TYPE       = 1
-local ADP_TYPE      = 2
-local BATTERY_TYPE  = 3
--- }}}
-
 -- {{{ Factory
 local function factory(args)
     args = args or {}
@@ -20,6 +13,7 @@ local function factory(args)
     local icons = args.icons or
         {
             ac = nil,
+            charging = nil,
             empty = nil,
             full = nil,
             logo = nil,
@@ -27,9 +21,10 @@ local function factory(args)
         }
     local info =
         {
-            power_supplies = {},
+            ac_connected = false,
+            batteries = {},
+            batteries_percentage = 0,
             power_supplies_updating = 0,
-            percentage = 0,
             status = "N/A"
         }
     local popup = awful.popup(
@@ -136,12 +131,15 @@ local function factory(args)
                     {
                         {
                             {
-                                align  = "center",
+                                background_color = beautiful.bg_focus,
+                                color = string.format("linear:0,0:150,0:0,%s:0.5,%s:1,%s", "#FF0000", "#FFFF00", "#00FF00"),
                                 forced_height = beautiful_dpi(20),
-                                forced_width = beautiful_dpi(64),
-                                text = string.format(" N/A"),
-                                valign = "center",
-                                widget = wibox.widget.textbox
+                                forced_width = beautiful_dpi(150),
+                                margins = beautiful_dpi(1),
+                                max_value = 100,
+                                paddings = beautiful_dpi(6),
+                                value = 0,
+                                widget = wibox.widget.progressbar
                             },
                             bg = beautiful.bg_focus,
                             shape = function(cr, width, height) gears.shape.rounded_rect(cr, beautiful_dpi(width), beautiful_dpi(height), beautiful_dpi(5)) end,
@@ -158,105 +156,95 @@ local function factory(args)
         )
     end
 
-    local function update_popup(power_info)
+    local function update_popup(bat_info)
         local function _set_text(container, index, value)
             local widget = container:get_children()[index]:get_children()[1]:get_children()[1]
             widget:set_markup(value)
         end
 
-        local rows_container_widget = power_info.popup.row:get_children()[1]
-        _set_text(rows_container_widget, 2, power_info.status)
-        _set_text(rows_container_widget, 3, string.format("%d%%", power_info.percentage))
+        local function _set_value(container, index, value)
+            local widget = container:get_children()[index]:get_children()[1]:get_children()[1]
+            widget:set_value(value)
+        end
+
+        local rows_container_widget = bat_info.popup.row:get_children()[1]
+        _set_text(rows_container_widget, 2, string.format("%d%%", bat_info.percentage))
+        _set_value(rows_container_widget, 3, bat_info.percentage)
     end
 
     local function update_widget()
         local icon_widget = widget:get_children_by_id("icon")[1]
         local text_widget = widget:get_children_by_id("text")[1]
         if (icon_widget ~= nil and text_widget ~= nil) then
-            if info.ac.status == true then
-                icon_widget:set_image(icons.ac)
-                text_widget:set_markup(" " .. info.percentage .. "%" .. " ")
+            if info.ac_connected == true then
+                icon_widget:set_image((#info.batteries == 0) and icons.ac or icons.charging)
+                text_widget:set_markup(" " .. info.batteries_percentage .. "%" .. " ")
             else
-                if info.percentage == 100 then
-                    icon_widget:set_image(icons.ac)
-                    text_widget:set_markup(" " .. info.percentage .. "%" .. " ")
-                elseif info.percentage > 50 then
+                if info.batteries_percentage > 50 then
                     icon_widget:set_image(icons.full)
-                    text_widget:set_markup(" " .. info.percentage .. "%" .. " ")
-                elseif info.percentage > 15 then
+                    text_widget:set_markup(" " .. info.batteries_percentage .. "%" .. " ")
+                elseif info.batteries_percentage > 15 then
                     icon_widget:set_image(icons.low)
-                    text_widget:set_markup(string.format("<span foreground='%s'> %s </span>", beautiful.fg_focus, info.percentage))
+                    text_widget:set_markup(string.format("<span foreground='%s'> %d%% </span>", beautiful.fg_focus, info.batteries_percentage))
                 else
                     icon_widget:set_image(icons.empty)
-                    text_widget:set_markup(string.format("<span foreground='%s'> %s </span>", beautiful.fg_urgent, info.percentage))
+                    text_widget:set_markup(string.format("<span foreground='%s'> %d%% </span>", beautiful.fg_urgent, info.batteries_percentage))
                 end
             end
         end
     end
 
-    local function update_ac_state(power_info)
-        power_info.percentage = 100
-        power_info.status = "Connected"
-
-        -- decrease the number of power supplies updating
-        -- info.power_supplies_updating = info.power_supplies_updating - 1
-
-        -- update popup
-        if popup.visible then
-            update_popup(power_info)
-        end
-    end
-
-    local function update_adp_state(power_info)
+    local function update_ac_state(name)
         awful.spawn.easy_async(
-            string.format("cat /sys/class/power_supply/%s/online", power_info.power_supply),
+            string.format("cat /sys/class/power_supply/%s/online", name),
             function(stdout, _, _, _)
                 -- read data from command results
-                power_info.status = (tonumber(stdout) == 1) and "On" or "Off"
+                info.ac_connected = info.ac_connected or (tonumber(stdout) == 1)
 
                 -- decrease the number of power supplies updating
                 info.power_supplies_updating = info.power_supplies_updating - 1
 
-                -- update popup
-                if popup.visible then
-                    update_popup(power_info)
+                -- update widget if all power supplies update has been completed
+                if info.power_supplies_updating == 0 then
+                    -- update widget
+                    update_widget()
                 end
             end
         )
     end
 
-    local function update_battery_state(power_info)
+    local function update_battery_state(bat_info)
         awful.spawn.easy_async(
             string.format("cat \
-                /sys/class/power_supply/%s/status \
+                /sys/class/power_supply/%s/energy_full \
                 /sys/class/power_supply/%s/energy_now \
-                /sys/class/power_supply/%s/energy_full",
-                power_info.power_supply,
-                power_info.power_supply,
-                power_info.power_supply),
+                /sys/class/power_supply/%s/status",
+                bat_info.name,
+                bat_info.name,
+                bat_info.name),
             function(stdout, _, _, _)
                 -- read data from command results
                 local i = 0
                 for v in stdout:gmatch("[^\r\n]+") do
                     if i == 0 then
-                        power_info.status = v
+                        bat_info.energy_full = tonumber(v)
                     elseif i == 1 then
-                        power_info.energy_now = tonumber(v)
+                        bat_info.energy_now = tonumber(v)
                     elseif i == 2 then
-                        power_info.energy_full = tonumber(v)
+                        bat_info.status = v
                     end
                     i = i + 1
                 end
 
                 -- calculate percentage
-                power_info.percentage = math.floor((power_info.energy_now / power_info.energy_full) * 100)
+                bat_info.percentage = math.floor((bat_info.energy_now / bat_info.energy_full) * 100)
 
                 -- decrease the number of power supplies updating
                 info.power_supplies_updating = info.power_supplies_updating - 1
 
                 -- update popup
                 if popup.visible then
-                    update_popup(power_info)
+                    update_popup(bat_info)
                 end
 
                 -- update widget if all power supplies update has been completed
@@ -264,11 +252,11 @@ local function factory(args)
                     -- calculate total percentage
                     local total_energy_full = 0
                     local total_energy_now = 0
-                    for _, v in pairs(info.power_supplies) do
+                    for _, v in pairs(info.batteries) do
                         total_energy_full = total_energy_full + v.energy_full
                         total_energy_now = total_energy_now + v.energy_now
                     end
-                    info.percentage = math.floor(((total_energy_now / total_energy_full) * 100) + 0.5)
+                    info.batteries_percentage = math.floor(((total_energy_now / total_energy_full) * 100) + 0.5)
 
                     -- update widget
                     update_widget()
@@ -281,6 +269,9 @@ local function factory(args)
         -- check that previous update has finished
         if info.power_supplies_updating == 0 then
 
+            -- reset ac connected flag
+            info.ac_connected = false
+
             -- check power supplies in the system
             awful.spawn.easy_async(
                 "ls /sys/class/power_supply",
@@ -291,58 +282,52 @@ local function factory(args)
                         table.insert(power_supplies, power_supply)
                     end
 
-                    -- if not power supply add default AC power supply
+                    -- if not power supply set default AC power supply
                     if #power_supplies == 0 then
-                        table.insert(power_supplies, "AC")
-                    end
+                        -- set ac connected
+                        info.ac_connected = true
 
-                    -- set number of power supplies updating
-                    info.power_supplies_updating = #power_supplies
+                        -- set number of power supplies updating to 1 to stop continous updates
+                        info.power_supplies_updating = 1
 
-                    -- iterate all power supplies
-                    for i, power_supply in ipairs(power_supplies) do
-                        if not info.power_supplies[i] then
-                            -- add new power supply info since there isn't any
-                            info.power_supplies[i] =
-                                {
-                                    energy_full = 0,
-                                    energy_now = 0,
-                                    percentage = 0,
-                                    popup =
-                                        {
-                                            row = create_popup_row(power_supply)
-                                        },
-                                    power_supply = power_supply,
-                                    status = "N/A",
-                                    type = UNKNOWN_TYPE
-                                }
-                            local power_info = info.power_supplies[i]
+                        -- update widget
+                        update_widget()
 
-                            -- set power supply type
-                            if string.match(power_supply, "AC") then
-                                power_info.type = AC_TYPE
-                            elseif string.match(power_supply, "ADP%w+") then
-                                power_info.type = ADP_TYPE
+                    else
+                        -- set number of power supplies updating
+                        info.power_supplies_updating = #power_supplies
+
+                        -- iterate all power supplies
+                        for i, power_supply in ipairs(power_supplies) do
+                            if string.match(power_supply, "A%w+") then
+                                -- check ac state
+                                update_ac_state(power_supply)
                             elseif string.match(power_supply, "BAT%w+") then
-                                power_info.type = BATTERY_TYPE
+                                if not info.batteries[i] then
+                                    -- add new battery info since there isn't any
+                                    info.batteries[i] =
+                                        {
+                                            energy_full = 0,
+                                            energy_now = 0,
+                                            name = power_supply,
+                                            percentage = 0,
+                                            popup =
+                                                {
+                                                    row = create_popup_row(power_supply)
+                                                },
+                                            status = "N/A"
+                                        }
+                                    local bat_info = info.batteries[i]
+
+                                    -- add popup row to layout
+                                    popup:get_widget():get_children_by_id("row_container")[1]:add(bat_info.popup.row)
+                                end
+
+                                local bat_info = info.batteries[i]
+
+                                -- check battery state
+                                update_battery_state(bat_info)
                             end
-
-                            -- add popup row to layout
-                            popup:get_widget():get_children_by_id("row_container")[1]:add(power_info.popup.row)
-                        end
-
-                        local power_info = info.power_supplies[i]
-
-                        -- update power supply state
-                        if power_info.type == AC_TYPE then
-                            -- check ac state
-                            update_ac_state(power_info)
-                        elseif power_info.type == ADP_TYPE then
-                            -- check adp state
-                            update_adp_state(power_info)
-                        elseif power_info.type == BATTERY_TYPE then
-                            -- check battery state
-                            update_battery_state(power_info)
                         end
                     end
                 end
@@ -352,20 +337,21 @@ local function factory(args)
 
     -- signals
     widget:connect_signal(
-        'mouse::enter',
+        "mouse::enter",
         function(c)
             c:set_bg(beautiful.bg_normal)
-
-            for _, v in pairs(info.power_supplies) do
-                update_popup(v)
+            if #info.batteries ~= 0 then
+                for _, v in pairs(info.batteries) do
+                    update_popup(v)
+                end
+                popup:move_next_to(mouse.current_widget_geometry)
+                popup.visible = true
             end
-            popup:move_next_to(mouse.current_widget_geometry)
-            popup.visible = true
         end
     )
 
     widget:connect_signal(
-        'mouse::leave',
+        "mouse::leave",
         function(c)
             popup.visible = false
             c:set_bg(beautiful.bg_reset)
